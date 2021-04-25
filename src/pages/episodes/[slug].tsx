e80beda614a7a29616f915c1590fa5298cbedc5f
michelewtc <michelechan1996@gmail.com>
2021-04-25 03:51:51 -0400

    day 4

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { GetStaticPaths, GetStaticProps } from 'next';
 
@@ -27,6 +28,15 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+    //fallback: true, //Requisicao pelo front-end
+    //verifica se esta em carregamento ou nao
+    
+    // const router = useRouter();
+
+    // if(router.isFallback){
+    //     return <p>Carregando...</p>
+    // }
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -62,8 +72,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params : {
+                slug: episode.id
+            }
+        }
+    })
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,5 @@
-// SPA - os dados carrega depois
-// import { useEffect } from "react"
-
-// export default function Home() {
-//   useEffect(() => {
-//     fetch('http://localhost:3333/episodes')
-//       .then(response => response.json())
-//       .then(data => console.log(data))
-//   }, [])
-  
-//   return (
-//     <h1>Index</h1>
-//   )
-// }
-
-
-
-// SSR - os dados ja vem carregados mesmo desabilitando JS
-// export default function Home(props) {
-//   return (
-//     <div>
-//       <h1>Index</h1>
-//       <p>{JSON.stringify(props.episodes)}</p>
-//     </div>
-//   )
-// }
-
-// export async function getServerSideProps() {
-//   const response = await fetch ('http://localhost:3333/episodes')
-//   const data = await response.json()
-
-//   return {
-//     props: {
-//       episodes: data,
-//     }
-//   }
-// }
-
-
 //SSG - Gera uma versão estática (html puro) - vai ter mesmo conteúdo gerado na 1 vez, sem solicitar uma nova requisição da API
-
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -47,6 +8,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -67,6 +29,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -92,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
 
-                  <button type="button">
+                  <button type="button" onClick={() => play(episode)}> 
                     <img src="/play-green.svg" alt="Tocar episódio"/>
                   </button>
                 </div>
